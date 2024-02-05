30eddef007cb86ae4cf1b4ff6fba6b631e2c8aad
SamuilDichev <samkata00@abv.bg>
2024-02-05 16:53:20 +0000

    Change null-ls to none-ls

--- .config/nvim/lua/samuild/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/samuild/plugins/lsp/null-ls.lua
@@ -1,5 +1,6 @@
 return {
-    "jose-elias-alvarez/null-ls.nvim",
+    -- "jose-elias-alvarez/null-ls.nvim", -- archived
+    "nvimtools/none-ls.nvim", -- fork of null-ls
     lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
 }
