25f20c2649a467d00cc00aa0a38f1cdf029934bb
SamuilDichev <samkata00@abv.bg>
2024-02-10 18:22:45 +0000

    Add autocmd for highlighting yanks

--- .config/nvim/lua/samuild/init.lua
+++ .config/nvim/lua/samuild/init.lua
@@ -11,3 +11,13 @@ vim.api.nvim_create_autocmd("WinLeave", {
         end
     end,
 })
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+    pattern = "*",
+    desc = "Highlight selection on yank",
+    callback = function()
+        vim.highlight.on_yank { timeout = 200, visual = true }
+    end,
+})
