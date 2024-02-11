3c08785b6842dadad8f3ee72fc75c511ba1394a5
SamuilDichev <samkata00@abv.bg>
2024-02-11 02:28:45 +0000

    Add Python snippets to LSP and improve order of suggestions

--- .config/nvim/lua/samuild/plugins/lsp/cmp.lua
+++ .config/nvim/lua/samuild/plugins/lsp/cmp.lua
@@ -2,19 +2,24 @@ return {
     "hrsh7th/nvim-cmp",
     lazy = false,
     dependencies = {
-        { "L3MON4D3/LuaSnip" },       -- source
-        { "hrsh7th/cmp-nvim-lsp" },   -- source
-        { "hrsh7th/cmp-buffer" },     -- source
-        { "hrsh7th/cmp-path" },       -- source
-        { "hrsh7th/cmp-cmdline" },    -- source
-        { "zbirenbaum/copilot-cmp" }, -- source
-        { "onsails/lspkind.nvim" },   -- icons in completion dropdown
+        { "L3MON4D3/LuaSnip" },             -- snippets engine
+        { "saadparwaiz1/cmp_luasnip" },     -- snippets source
+        { "rafamadriz/friendly-snippets" }, -- snippets
+
+        { "hrsh7th/cmp-nvim-lsp" },         -- source
+        { "hrsh7th/cmp-buffer" },           -- source
+        { "hrsh7th/cmp-path" },             -- source
+        { "hrsh7th/cmp-cmdline" },          -- source
+        { "zbirenbaum/copilot-cmp" },       -- source
+
+        { "onsails/lspkind.nvim" },         -- icons in completion dropdown
     },
     config = function()
         local cmp = require('cmp')
         local lspkind = require("lspkind")
         local mapping = require("samuild.keymap").add_lsp_suggestion_keymap(cmp)
         require("copilot_cmp").setup({})
+        require("luasnip.loaders.from_vscode").lazy_load()
 
         -- Completion global settings
         cmp.setup({
@@ -35,13 +40,30 @@ return {
                 completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
             },
+            sorting = {
+                priority_weight = 1.0,
+                comparators = {
+                    cmp.config.compare.locality,
+                    cmp.config.compare.recently_used,
+                    cmp.config.compare.score,
+                    cmp.config.compare.offset,
+                    cmp.config.compare.order,
+                    -- cmp.config.compare.exact,
+                    -- cmp.config.compare.kind,
+                    -- cmp.config.compare.length,
+                    -- cmp.config.compare.scopes,
+                    -- cmp.config.compare.score_offset,
+                    -- cmp.config.compare.sort_text,
+                },
+            },
             sources = {
-                { name = 'nvim_lsp' },
-                { name = 'nvim_lua' },
-                { name = 'copilot' },
+                { name = 'copilot',  priority = 8 },
+                { name = 'nvim_lsp', priority = 8 },
+                { name = 'luasnip',  priority = 7 },
+                { name = "buffer",   priority = 7 },
+                { name = 'nvim_lua', priortiy = 5 },
                 -- { name = "treesitter" },
-                { name = "buffer" },
-                { name = 'path' },
+                -- { name = 'path' },
             },
             mapping = cmp.mapping.preset.insert(mapping)
         })
