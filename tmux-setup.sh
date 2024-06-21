653e538b67f84f881d6596a80889fa1126109674
SamuilDichev <samkata00@abv.bg>
2024-06-21 11:29:17 +0100

    Add tmux setup script

---
+++ tmux-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+repos_path="$HOME/repos"
+
+# Function to check if a session exists
+session_exists() {
+    tmux has-session -t "$1" 2>/dev/null
+}
+
+# Function to create a session if it doesn't exist
+create_session() {
+    local session_name="$1"
+    local session_path="$2"
+
+    if ! session_exists "$session_name"; then
+        tmux new-session -d -s "$session_name" -c "$session_path"
+        tmux split-window -h -c "$session_path"
+	tmux split-window -v -c "$session_path"
+	tmux select-pane -t 1
+    fi
+}
+
+# General
+create_session "dotfiles" "$repos_path/dotfiles"
+create_session "General" "~"
+
+# Work
+create_session "Beefy" "$repos_path/beefy"
