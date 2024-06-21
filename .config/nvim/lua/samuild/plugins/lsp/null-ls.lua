72afcbe2347a201f27249f7fe8e92424a572d67a
SamuilDichev <samkata00@abv.bg>
2024-06-21 13:01:10 +0100

    Replace black and isort with ruff

--- .config/nvim/lua/samuild/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/samuild/plugins/lsp/null-ls.lua
@@ -2,13 +2,22 @@ return {
     -- "jose-elias-alvarez/null-ls.nvim", -- archived
     "nvimtools/none-ls.nvim", -- fork of null-ls
     lazy = false,
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvimtools/none-ls-extras.nvim",
+    },
     config = function()
         local null_ls = require("null-ls")
-        local b = null_ls.builtins
+        -- local isort = null_ls.builtins.formatting.isort
+        -- local black = null_ls.builtins.formatting.black
+        local ruff_lint = require("none-ls.formatting.ruff")
+        local ruff_format = require("none-ls.formatting.ruff_format")
+
         local sources = {
-            b.formatting.isort,
-            b.formatting.black.with({ extra_args = { "--line-length", "120" } }),
+            -- isort,
+            -- black.with({ extra_args = { "--line-length", "120" } }),
+            ruff_lint.with({ extra_args = { "--select", "I" } }),
+            ruff_format.with({ extra_args = { "--line-length", "120" } }),
         }
 
         null_ls.setup({ sources = sources })
