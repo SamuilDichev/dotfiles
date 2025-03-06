9d1fecadef8d3513a5d14255783eb8e984e4d93b
SamuilDichev <samkata00@abv.bg>
2025-03-06 12:50:49 +0000

    Disable Folding

--- .config/nvim/lua/samuild/plugins/ufo.lua
+++ .config/nvim/lua/samuild/plugins/ufo.lua
@@ -1,17 +1,17 @@
 return {
-    "kevinhwang91/nvim-ufo",
-    dependencies = {
-        "kevinhwang91/promise-async",
-    },
-    lazy = false,
-    config = function()
-        local ufo = require("ufo")
-        require("samuild.keymap").set_folding_keymap(ufo)
-
-        ufo.setup({
-            provider_selector = function(bufnr, filetype, buftype)
-                return { "lsp", "indent" }
-            end
-        })
-    end
+    -- "kevinhwang91/nvim-ufo",
+    -- dependencies = {
+    --     "kevinhwang91/promise-async",
+    -- },
+    -- lazy = false,
+    -- config = function()
+    --     local ufo = require("ufo")
+    --     require("samuild.keymap").set_folding_keymap(ufo)
+    --
+    --     ufo.setup({
+    --         provider_selector = function(bufnr, filetype, buftype)
+    --             return { "lsp", "indent" }
+    --         end
+    --     })
+    -- end
 }
